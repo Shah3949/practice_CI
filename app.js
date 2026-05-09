507ee6b679ee0e763a3560633cca72da253d5c4a
Shah3949 <shahzebsalahuddin0@gmail.com>
2026-05-09 11:17:52 +0530

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 // Serve static files (CSS, Images) from a folder named 'public'
 app.use(express.static("public"));
